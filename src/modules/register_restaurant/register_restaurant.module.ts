04fb8db962f79f0c0fd59a935049bebf32450ede
Adriano Hilario <adrianoferreira31@gmail.com>
2023-05-08 11:46:10 -0300

    :construction: creating search of all restaurant

--- src/modules/register_restaurant/register_restaurant.module.ts
+++ src/modules/register_restaurant/register_restaurant.module.ts
@@ -3,10 +3,16 @@ import { RegisterRestaurantController } from './register_restaurant.controller';
 import { Restaurant_register } from './services/restaurant_register.service';
 import { RestaurantRepository } from './repository/restaurant.repositoy';
 import { PrismaService } from 'src/database/clientPrisma';
+import { Restaurant_getAll } from './services/get_restaurant_all.service';
 
 @Module({
   controllers: [RegisterRestaurantController],
-  providers: [Restaurant_register, RestaurantRepository, PrismaService],
+  providers: [
+    Restaurant_register,
+    Restaurant_getAll,
+    RestaurantRepository,
+    PrismaService,
+  ],
   exports: [Restaurant_register],
 })
 export class RegisterRestaurantModule {}
